e69d222d279cae61985e730e89f8a9ba6d874fcb
Grant <virtualflyinginstagram@gmail.com>
2020-05-15 15:35:05 -0500

    Fixed connection to DB

--- index.php
+++ index.php
@@ -5,10 +5,9 @@
     <title>FSGateFinder</title>
 <<link rel="stylesheet" href="styles.css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
   </head>
   <body>
-
+<h1>FSGateFinder</h1>
     <form action="search.php" method="GET">
         <input type="text" name="query" />
         <input type="submit" value="Search" />
